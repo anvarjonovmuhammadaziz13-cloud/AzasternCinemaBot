6e788fd13e1e1bfc6df299665a2c0d9322f5230b
anvarjonovmuhammadaziz13-cloud <anvarjonovmuhammadaziz13@gmail.com>
2026-07-12 12:03:03 +0500

    Update config.py

--- config.py
+++ config.py
@@ -2,12 +2,8 @@ import os
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 
-# Majburiy obuna kanallari
 CHANNELS = [
-    "@1kanaling_username",
-    "@2kanaling_username",
-    "@3kanaling_username"
+    "@kanal1",
+    "@kanal2",
+    "@kanal3",
 ]
-
-# Admin Telegram ID
-ADMIN_ID = 123456789
